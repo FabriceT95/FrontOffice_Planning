3a0908660cf8b1e0b1d9222693b2dd5ed5363a3b
FabriceT95 <fabrice.tapia95@gmail.com>
2022-12-14 12:39:17 +0100

    Basic Project Structure; 14/12/2022

---
+++ src/test/java/com/example/frontoffice_planning/FrontOfficePlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontoffice_planning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontOfficePlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
